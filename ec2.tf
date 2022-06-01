827f04b04bee8dc5dfe1088331e7e13d6dabe664
Vinicius Figueira <viniciusfigueira@pop-os.localdomain>
2022-05-31 21:59:04 -0300

    v1

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = var.name
+    Environment = var.env
+    Provisioner = "Terraform"
+    Repo        = var.repo
+
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  description = "Define what region the instance will be deployed"
+  default     = "us-east-2"
+}
+
+variable "name" {
+  description = "Name of the application"
+  default     = "server01"
+}
+
+variable "env" {
+  description = "Environment of the application"
+  default     = "dev"
+}
+
+
+variable "instance_type" {
+  description = "AWS Instance type defines the hardware configuration of the machine"
+  default     = "t2.micro"
+}
+
+variable "repo" {
+  description = "Repository of the application"
+  default     = "https://github.com/viniciusfigueira"
+}
